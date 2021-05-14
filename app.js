1285fbb9eacce24b50cea54e41fb09aa2768c810
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-05-14 03:42:53 -0700

    amc

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const fs = require('fs')
-const generatePage = require("./src/page-template")
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+   }
+ ])
+ .then(answers => console.log(answers))
+//const fs = require('fs')
+//const generatePage = require("./src/page-template")
+//const pageHTML = generatePage(name, github)
+
 
 //const printProfileData = (profileDataArr) => {
    // for (let i=0 ; i < profileDataArr.length; i++) {
@@ -17,8 +28,8 @@ const [name, github] = profileDataArgs;
 
 //printProfileData(profileDataArgs);
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-    if (err) throw err;
+//fs.writeFile('index.html', pageHTML, err => {
+    //if (err) throw err;
 
-    console.log("Portfolio complete ! checkout index.html for the output")
-})
+    //console.log("Portfolio complete ! checkout index.html for the output")
+//})
