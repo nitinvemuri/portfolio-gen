e25db3fe7f863f5cf4e15b078cce1ccf14c60240
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-05-15 07:15:44 -0700

    last commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
-const fs = require('fs');
-const generatePage = require('./page-template');
+//const fs = require('fs');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile} = require('./utils/generate-site')
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -126,18 +127,25 @@ const promptProject = portfolioData => {
     }
   });
 }
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
-
-    console.log('Page created! Check out index.html in this directory to see it!');
-    })
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
 
 
 
@@ -213,4 +221,4 @@ promptUser()
     //if (err) throw err;
 
     //console.log("Portfolio complete ! checkout index.html for the output")
-//})
+//};
